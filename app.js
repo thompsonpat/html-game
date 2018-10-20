7e344111d4c93571f182184f905f01057b5b34bc
thompsonpat <thompsonpat21@gmail.com>
2018-10-20 03:29:48 -0400

    fixed __dirname in app.js

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ var serv = require('http').Server(app);
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
-app.use('/client', express.static(_dirname + '/client'));
+app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(2000);
